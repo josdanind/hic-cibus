9aac6c0dac85128d2d0bc27209e49f04fbb8ae37
josdanind <josdanind@protonmail.com>
2025-10-21 08:44:47 -0500

    Se amplio a 25 la cantidad de caracteres para un buttoninline

--- services/postgresql/models/tlaloc_db/01_tlaloc_core_models_v0.2.0.sql
+++ services/postgresql/models/tlaloc_db/01_tlaloc_core_models_v0.2.0.sql
@@ -1422,7 +1422,7 @@ CREATE TABLE process_reel_links (
                         REFERENCES reels(id)
                         ON DELETE CASCADE,
 
-    process_name        VARCHAR(15)                 NOT NULL,
+    process_name        VARCHAR(25)                 NOT NULL,
 
     -- Auditoría
     created_at          TIMESTAMPTZ                 NOT NULL DEFAULT now(),
